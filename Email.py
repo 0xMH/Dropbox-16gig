f40ccb7abd72a3b878f6877a8276b5b913cc2fdc
Mohamed Hamza <hamza@Mohameds-MacBook-Pro.local>
2017-06-28 18:53:53 +0300

    EmailNames generation added

--- Email.py
+++ Email.py
@@ -1,36 +1,48 @@
 from hashlib import md5
 import requests
+import string
 from random import choice
-import time
 
-#http://api.temp-mail.ru/request/domains/
 
-class Email:
+class Emails:
     '''a simple wrapper for  api.temp-mail.ru.
     I'll use this Emails in the singing in process'''
 
 
-    def __init__(self, login):
-        self.login = login
+    def __init__(self, login=None):
+        if login:
+            self.login = login
+        else:
+            self.login = self.__name_gen()
 
     def get_hash(self, intity):
-        ''' get the hashes of the emails'''
+        """get the hashes of the emails"""
         return md5(intity.encode('utf-8')).hexdigest()
 
-    def __get_Domains(self):
-        '''ge the availabe domains and choose one randomly'''
+    def __get_domains(self):
+        """ge the availabe domains and choose one randomly"""
 
         domains = requests.get('http://api.temp-mail.ru/request/domains/format/json').json()
         return choice(domains)
 
     def new_email(self):
+        """make a new Email"""
 
-        setattr(self,'email',self.login + self.__get_Domains())
+        setattr(self,'email',self.login + self.__get_domains())
         return self.email
 
     def get_messages(self):
+        """get the mail box"""
+
         return requests.get('http://api.temp-mail.ru/request/mail/id/{}/format/json/'
                             .format(self.get_hash(self.email))).json()
 
+    def __password_gen(self):
+        pass
 
+    def __name_gen(self):
+        """generate random names for Emails"""
+
+        chars = string.ascii_letters + string.digits
+        return ''.join(choice(chars) for _ in range(8))
 
